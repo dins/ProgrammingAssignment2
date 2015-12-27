b8001d6650a4e63715ce9e8d97bcc3b09f09be22
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2015-12-27 18:48:10 +0200

    implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 solution with functions makeCacheMatrix that 
+## creates a cacheing object and cacheSolve that can invert a matrix inside
+## such object.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a object that is able to cache an output value, 
+## but does not allow to mutate the first value.
+makeCacheMatrix <- function(input = matrix()) {
+  cachedOutput <- NULL
+  cachedInput <<- input
+  getInput <- function() cachedInput
+  setOutput <- function(output) cachedOutput <<- output
+  getOutput <- function() cachedOutput
+  list(getInput = getInput,
+       setOutput = setOutput,
+       getOutput = getOutput)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Invert the matrix in cacheMatrix usign the ... params. 
+## If cached value exists the that is returned. 
+## cacheMatrix must be created using makeCacheMatrix.
+cacheSolve <- function(cacheMatrix, ...) {
+  inverse <- cacheMatrix$getOutput()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- cacheMatrix$getInput()
+  inverse <- solve(data, ...)
+  cacheMatrix$setOutput(inverse)
+  inverse
 }
